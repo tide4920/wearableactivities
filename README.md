da70303c5cfd1cafd32a0b82f3173b2eb8ee3835
Murali Niverthi <murali.niverthi@gmail.com>
2016-02-02 21:50:51 -0500

    First commit

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # wearableactivities
 
-# the wearableactivities repository includes 
-# 1 run_analysis.R code
+# the wearableactivities repository includes 3 files 
+# file # 1 run_analysis.R code
 # the code produces a tidy data set with one row for each activity / subject / variable / mean for the variable
+# the final wearableactivities.txt had 11,880 rows and 4 columns, in a long format
+# the columns are
+# 1 - activity
+# 2 - subject 
+# 3 - variable - details are in the codebook file
+# 4 - average of variable for that activity / subject over the multiple observations
+
+# file # 2 codebook file
+# has details of the 66 variable names included in the final tidy data set
+
+# file # 3 this READme.md file
--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,12 @@
 # Course 3A January 2016 
 # course assignment
 
+
+# steps 1 to 4
 Xtrain <- read.table("./Coursera/X_train.txt")
 dim(Xtrain)
 
+# selecting only columns with mean(), std() in their name based on features.txt
 Xtrain <- Xtrain[, c(1:6, 41:46, 81:86, 121:126, 161:166, 201:202, 214:215, 227:228, 240:241, 253:254,
 266:271, 345:350, 424:429, 503:504, 516:517, 529:530, 542:543) ]
 dim(Xtrain)
@@ -11,6 +14,7 @@ dim(Xtrain)
 Xtest <- read.table("./Coursera/X_test.txt")
 dim(Xtest)
 
+# selecting only columns with mean(), std() in their name based on features.txt
 Xtest <- Xtest[, c(1:6, 41:46, 81:86, 121:126, 161:166, 201:202, 214:215, 227:228, 240:241, 253:254,
 266:271, 345:350, 424:429, 503:504, 516:517, 529:530, 542:543) ]
 dim(Xtest)
@@ -41,59 +45,59 @@ dim(test)
 # these variables do not have separate labels 
 # although description exists in features_info.txt
 
-bodyaccx_test <- read.table("./Coursera/body_acc_x_test.txt")
-dim(bodyaccx_test)
+#bodyaccx_test <- read.table("./Coursera/body_acc_x_test.txt")
+#dim(bodyaccx_test)
 
-bodyaccy_test <- read.table("./Coursera/body_acc_y_test.txt")
-dim(bodyaccy_test)
+#bodyaccy_test <- read.table("./Coursera/body_acc_y_test.txt")
+#dim(bodyaccy_test)
 
-bodyaccz_test <- read.table("./Coursera/body_acc_z_test.txt")
-dim(bodyaccz_test)
+#bodyaccz_test <- read.table("./Coursera/body_acc_z_test.txt")
+#dim(bodyaccz_test)
 
-bodygyrox_test <- read.table("./Coursera/body_gyro_x_test.txt")
-dim(bodygyrox_test)
+#bodygyrox_test <- read.table("./Coursera/body_gyro_x_test.txt")
+#dim(bodygyrox_test)
 
-bodygyroy_test <- read.table("./Coursera/body_gyro_y_test.txt")
-dim(bodygyroy_test)
+#bodygyroy_test <- read.table("./Coursera/body_gyro_y_test.txt")
+#dim(bodygyroy_test)
 
-bodygyroz_test <- read.table("./Coursera/body_gyro_z_test.txt")
-dim(bodygyroz_test)
+#bodygyroz_test <- read.table("./Coursera/body_gyro_z_test.txt")
+#dim(bodygyroz_test)
 
-totaccx_test <- read.table("./Coursera/total_acc_x_test.txt")
-dim(totaccx_test)
+#totaccx_test <- read.table("./Coursera/total_acc_x_test.txt")
+#dim(totaccx_test)
 
-totaccy_test <- read.table("./Coursera/total_acc_y_test.txt")
-dim(totaccy_test)
+#totaccy_test <- read.table("./Coursera/total_acc_y_test.txt")
+#dim(totaccy_test)
 
-totaccz_test <- read.table("./Coursera/total_acc_z_test.txt")
-dim(totaccz_test)
+#totaccz_test <- read.table("./Coursera/total_acc_z_test.txt")
+#dim(totaccz_test)
 
-bodyaccx_train <- read.table("./Coursera/body_acc_x_train.txt")
-dim(bodyaccx_train)
+#bodyaccx_train <- read.table("./Coursera/body_acc_x_train.txt")
+#dim(bodyaccx_train)
 
-bodyaccy_train <- read.table("./Coursera/body_acc_y_train.txt")
-dim(bodyaccy_train)
+#bodyaccy_train <- read.table("./Coursera/body_acc_y_train.txt")
+#dim(bodyaccy_train)
 
-bodyaccz_train <- read.table("./Coursera/body_acc_z_train.txt")
-dim(bodyaccz_train)
+#bodyaccz_train <- read.table("./Coursera/body_acc_z_train.txt")
+#dim(bodyaccz_train)
 
-bodygyrox_train <- read.table("./Coursera/body_gyro_x_train.txt")
-dim(bodygyrox_train)
+#bodygyrox_train <- read.table("./Coursera/body_gyro_x_train.txt")
+#dim(bodygyrox_train)
 
-bodygyroy_train <- read.table("./Coursera/body_gyro_y_train.txt")
+#bodygyroy_train <- read.table("./Coursera/body_gyro_y_train.txt")
 dim(bodygyroy_train)
 
-bodygyroz_train <- read.table("./Coursera/body_gyro_z_train.txt")
-dim(bodygyroz_train)
+#bodygyroz_train <- read.table("./Coursera/body_gyro_z_train.txt")
+#dim(bodygyroz_train)
 
-totaccx_train <- read.table("./Coursera/total_acc_x_train.txt")
-dim(totaccx_train)
+#totaccx_train <- read.table("./Coursera/total_acc_x_train.txt")
+#dim(totaccx_train)
 
-totaccy_train <- read.table("./Coursera/total_acc_y_train.txt")
-dim(totaccy_train)
+#totaccy_train <- read.table("./Coursera/total_acc_y_train.txt")
+#dim(totaccy_train)
 
-totaccz_train <- read.table("./Coursera/total_acc_z_train.txt")
-dim(totaccz_train)
+#totaccz_train <- read.table("./Coursera/total_acc_z_train.txt")
+#dim(totaccz_train)
 
 # end of section that does not get used
 
@@ -109,18 +113,17 @@ activity_subject <- append("activity", "subject")
 activity_subject
 total_features <- features[c(1:6, 41:46, 81:86, 121:126, 161:166, 201:202, 214:215, 227:228, 240:241, 253:254,
 266:271, 345:350, 424:429, 503:504, 516:517, 529:530, 542:543),2]
-total_features
 names(total) <- append(activity_subject, total_features)
 
 activities <- read.table("./Coursera/activity_labels.txt",stringsAsFactors=FALSE)
 dim(activities)
 activities
 
+# converting integer activity variable to a decriptive factor variable
 class(total$activity)
 total$activity <- factor(total$activity)
 class(total$activity)
 table(total$activity)
-
 levels(total$activity) <- c("1"="walking", "2"="walking upstairs", "3"="walking downstairs", "4"="sitting", "5"="standing", "6"="laying")
 table(total$activity)
 
@@ -128,37 +131,41 @@ names(total)
 table(total$activity)
 table(total$subject)
 
-# this ends steps 1-4 
-# now create second independent tidy data set with average for each parameter by activity, subject
-
+length(names(total))
 table(total$activity, total$subject)
 
+# this ends steps 1-4 
+
+# now create second independent tidy data set with average for each parameter by activity, subject
+# step 5 - create tidy data set 
+
 library(reshape2)
-length(names(total))
 
 var.out <- names(total)[!names(total) %in% c("activity", "subject")]
+length(var.out)
 mean_total <- melt(total, id=c("activity", "subject"), measure.vars=var.out)
 dim(mean_total)
 
-#mean_total[which(mean_total$activity=="walking" & mean_total$subject==1 & 
-#head(mean_total, 200)
-#total[which(total$activity=="laying" & total$subject==30), c(1,2,3)]
-
+# mean_total is a very long data set with each activity / subject / variable
+# combination having multiple observations 
+# the final tidy data set needs to have 1 observation for each activity / subject /variable combination
 
 # use plyr package
 library(plyr)
 finaldata <- ddply(mean_total, .(activity, subject, variable), summarize, average = mean(value))
-tail(finaldata,66)
+# the final tidy data set - finaldata gets printed down below
 dim(finaldata)
+# replacing the leading t, f in variable names by time and frequency to make names descriptive
+finaldata$variable <- gsub("^t", "time", finaldata$variable)
+finaldata$variable <- gsub("^f", "frequency", finaldata$variable)
+table(finaldata$variable)
 finaldata
+names(finaldata)
+dim(finaldata)
 
-# use dcast
-# newData <- dcast(mean_total, .(activity, subject) ~ variable, mean) 
-# dcast does not address the issue as it sets up all the variables as sideways columns
-# not what we want
+# the final tidy data set has 11,880 rows and 4 columns
 
 # write finaldata file to a table to upload
 
-#library(data.table)
-
-#write.table(finaldata, "./Coursera/wearableactivity.txt", row.names=FALSE, col.names=TRUE, sep="\t", quote=FALSE)
+library(data.table)
+write.table(finaldata, "./Coursera/wearableactivity.txt", row.names=FALSE, col.names=TRUE, sep="\t", quote=FALSE)
